beaffb3d2f8c015d8ba6617162603de94d0727ea
rjmarin <rjmarin@miuandes.cl>
2019-04-17 11:06:16 -0400

    advance

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,18 @@ class RegistrationsController < ApplicationController
 	end
 
 	def create
-	    #complete this method
+		@user = User.create(name: params[:registrations][:name],last_name:params[:registrations][:last_name],
+												email:params[:registrations][:email],
+												phone:params[:registrations][:phone],password:params[:registrations][:password])
+
+			if @user.save
+				respond_to do |format|
+					format.html { redirect_to root_path, notice: 'User was successfully created.' }
+					end
+			else
+				flash[:error] = "couldn't create the user"
+				redirect_to sign_in
+			end
+
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,20 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		#complete this method
+		user = User.where(email: params[:session][:email]).first
+		if user  && user.password == params[:session][:password]
+			flash[:notice] = "login is sussesfully"
+			redirect_to "/users/" + String(user.id)
+		else
+			flash[:error] = 'error email or password are incorrect'
+			redirect_to root_path
+		end
+
+
+
+
+
+
 	end
 
 	def destroy
